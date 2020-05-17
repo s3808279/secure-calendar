952b9a12589f168fdea68b55cc8b7f432ce02836
Stewart Jones <s3808279@student.rmit.edu.au>
2020-05-17 10:45:20 +1000

    Add activity_home

---
+++ app/src/main/java/com/ulan/timetable/activities/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.ulan.timetable.activities;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ulan.timetable.R;
+
+public class HomeActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_home);
+        new Handler().postDelayed (new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
